caa1ed484fb684bc46253e553f20c1d47ecbf97f
unvise0 <vinnikovmd17@gmail.com>
2022-02-17 22:43:52 +0300

    Add consts to solution.go

--- solution.go
+++ solution.go
@@ -4,13 +4,19 @@ import "math"
 
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
-type SidesNum int
-
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
 // it's like:
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+
+type SidesNum int
+
+const (
+	SidesCircle   SidesNum = 0
+	SidesTriangle SidesNum = 3
+	SidesSquare   SidesNum = 4
+)
 
 func CalcSquare(sideLen float64, sidesNum SidesNum) float64 {
 	if int(sidesNum) == 4 {
